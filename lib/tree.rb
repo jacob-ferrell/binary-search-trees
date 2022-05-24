b00daf8f9e23ef15bd423bffd37353d371d94f61
jacob-ferrell <michaeljacobferrell@gmail.com>
2022-05-24 16:38:09 -0400

    added method for inserting nodes with given value into tree

--- lib/tree.rb
+++ lib/tree.rb
@@ -4,20 +4,45 @@ class Tree
   attr_accessor :root
 
   def initialize(array)
-    p array
     @array = array.uniq.sort { |a, b| a - b }
-    p @array
     @root = build_tree(@array, 0, @array.length - 1)
   end
 
   def build_tree(array, start, last)
-    return if start > last
+    return nil if start > last
     mid = (start + last) / 2
     root = Node.new(array[mid])
     root.left_child = build_tree(array, start, mid - 1)
     root.right_child = build_tree(array, mid + 1, last)
     return root
   end
+
+  def insert(value)
+    return if @array.include?(value)
+    node = @root
+    while node
+      if value < node.data
+        if node.left_child
+          node = node.left_child
+        else
+          node.left_child = Node.new(value)
+          @array << value
+          return
+        end
+      else
+        if
+          node.right_child
+          node = node.right_child
+        else
+          node.right_child = Node.new(value)
+          @array << value
+          return
+        end
+      end
+    end
+  end
 end
 
-print Tree.new([9,8,7,6,5,4,3,2,1]).root.right_child.data
+t = Tree.new([9,8,7,6,5,4,3,2,1])
+t.insert(0)
+p t.root.left_child.left_child.left_child.data
